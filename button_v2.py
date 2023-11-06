ff39ca82877d8d5bf21138bb41f8c89e160c47a3
Boaztheostrich <zaob172@gmail.com>
2023-11-06 16:05:49 -0600

    first push button v2 is the latest code I don't like that the code is in another folder

---
+++ button_v2.py
@@ -0,0 +1,28 @@
+from picamera2 import Picamera2, Preview
+import time
+from datetime import datetime
+from gpiozero import Button
+from signal import pause
+picam2 = Picamera2()
+button = Button(16)
+camera_config = picam2.create_preview_configuration(main={"size": (4056, 3040)}, lores={"size": (480, 360)}, display="lores", buffer_count=5)
+picam2.configure(camera_config)
+
+def capture():
+    with open("picture_count.txt", "r") as file:
+        number = int(file.read())
+
+    number += 1
+
+    metadata = picam2.capture_file("/home/boaz/Desktop/images/" + "pibz_" + str(number) +".jpg")
+
+    with open("picture_count.txt", "w") as file:
+        file.write(str(number))
+
+    print(metadata)
+
+picam2.start_preview(Preview.QTGL, width=549, height=412)
+picam2.start()
+
+while True:
+    button.when_pressed = capture
---
+++ bzcamv0.py
@@ -0,0 +1,48 @@
+
+import sys
+import select
+import time
+from gpiozero import Button
+from picamera2 import Picamera2, Preview
+
+# Button is connected to GPIO 16
+
+button = Button(16)
+
+# Initialize the camera and preview outside the loop
+picam2 = Picamera2()
+camera_config = picam2.create_still_configuration(main={"size": (4056, 3040)}, lores={"size": (640, 480)}, display="lores")
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+
+while True:
+    # Loop to wait on request
+    request = "none"
+
+    while True:
+        if button.is_pressed:
+            request = "capture"
+            break
+
+        key_input = select.select([sys.stdin], [], [], 1)[0]
+
+        if key_input:
+            key_value = sys.stdin.readline().rstrip()
+
+            if key_value == "q":
+                request = "quit"
+                break
+            else:
+                request = "capture"
+                break
+
+    if request == "quit":
+        break
+
+    # Capture the image
+    metadata = picam2.capture_file("/home/boaz/Desktop/images/" + time.strftime("%Y%m%d-%H%M%S") + ".jpg")
+    print(metadata)
+
+# Close the camera at the end
+picam2.close()
